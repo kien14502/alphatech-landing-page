ef651b433fc6d27d889b90c5918752c8c8c41aeb
kien14502 <phankien.epu@gmail.com>
2025-12-01 10:25:26 +0700

    update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,6 +3,7 @@
 import ContactForm from '@/components/common/ContactForm';
 import {SectionTitle} from '@/components/common/SectionTitle';
 import {Button} from '@/components/ui/button';
+import {motion} from 'motion/react';
 import {useRef} from 'react';
 
 const ContactPage = () => {
@@ -16,17 +17,36 @@ const ContactPage = () => {
 	};
 
 	return (
-		<div>
-			<div className="w-screen flex flex-col justify-center items-center h-[calc(100vh-var(--header-height))] mt-(--header-height)">
-				<h1 className="text-7xl font-bold">Have Any Doubts? We</h1>
-				<h1 className="text-7xl font-bold">are Ready to Help.</h1>
+		<div className="flex flex-col px-10 sm:px-0 w-screen items-center">
+			<div
+				style={{
+					backgroundImage: "url('/images/banner.png')",
+					backgroundSize: 'cover',
+					backgroundPosition: 'center',
+					backgroundRepeat: 'no-repeat',
+				}}
+				className="w-screen flex flex-col justify-center items-center h-screen">
+				<motion.p
+					initial={{opacity: 0, y: -20}}
+					animate={{opacity: 1, y: 0}}
+					transition={{duration: 0.6, ease: 'easeOut', delay: 0.2}}
+					className="text-7xl font-bold text-white text-shadow-2xs">
+					Have Any Doubts? We
+				</motion.p>
+				<motion.p
+					initial={{opacity: 0, y: -20}}
+					animate={{opacity: 1, y: 0}}
+					transition={{duration: 0.6, ease: 'easeOut', delay: 0.2}}
+					className="text-7xl font-bold text-white text-shadow-2xs">
+					are Ready to Help.
+				</motion.p>
 				<Button onClick={scrollToForm} className="mt-6">
 					Fill The Form Out!
 				</Button>
 			</div>
 			<div
 				ref={formRef}
-				className="w-screen h-screen flex flex-col gap-12 items-center justify-center">
+				className="flex flex-col w-full h-screen gap-12 items-center justify-center">
 				<SectionTitle title="Contact Us" />
 				<ContactForm />
 			</div>
--- components/common/HomeBanner.tsx
+++ components/common/HomeBanner.tsx
@@ -6,19 +6,26 @@ import {motion} from 'motion/react';
 
 const HomeBanner = () => {
 	return (
-		<div className="w-screen h-screen flex flex-col items-center justify-center">
+		<div
+			style={{
+				backgroundImage: "url('/images/banner.png')",
+				backgroundSize: 'cover',
+				backgroundPosition: 'center',
+				backgroundRepeat: 'no-repeat',
+			}}
+			className="w-screen h-screen flex flex-col items-center justify-center">
 			<motion.p
 				initial={{opacity: 0, y: -20}}
 				animate={{opacity: 1, y: 0}}
 				transition={{duration: 0.6, ease: 'easeOut', delay: 0.2}}
-				className="text-5xl font-bold text-center">
+				className="text-5xl font-bold text-center text-white text-shadow-2xs">
 				Better Technology
 			</motion.p>
 			<motion.p
 				initial={{opacity: 0, y: -20}}
 				animate={{opacity: 1, y: 0}}
 				transition={{duration: 0.6, ease: 'easeOut', delay: 0.2}}
-				className="text-5xl font-bold text-center">
+				className="text-5xl font-bold text-center text-white text-shadow-2xs">
 				Better Life
 			</motion.p>
 
